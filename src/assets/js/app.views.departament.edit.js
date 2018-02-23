e0f0bd68d6246401bd2c8a02ec61e0bba9b63db2
Willyam Santana Almeida <willyam.almeida@lggo.com.br>
2018-02-23 17:58:13 -0300

    add db storage

--- src/assets/js/app.views.departament.edit.js
+++ src/assets/js/app.views.departament.edit.js
@@ -5,14 +5,37 @@ app.views.departament.edit = function($el) {
     }
 
     this.$el = $el;
+    this.db = new db("departaments");
+
     this.init();
 };
 
 app.views.departament.edit.prototype = {
-    init: function() {       
+    init: function() {    
+        this.$elSave = this.$el.parentElement.querySelector("[name='save']");
+        this.$elDelete = this.$el.parentElement.querySelector("[name='delete']");
+        this.$elCancel = this.$el.parentElement.querySelector("[name='cancel']");
+
+        this.$elId = this.$el.parentElement.querySelector("[name='id']");
+        this.$elDescription = this.$el.parentElement.querySelector("[name='description']");
+
         this.initEvents();
     },
 
-    initEvents: function() {        
+    save: function() {        
+        let departament = {
+            id: this.$elId.value,
+            description: this.$elDescription.value
+        };
+
+        this.db.save(departament);
+    },
+
+    initEvents: function() {       
+        let _this = this;
+        
+        this.$elSave.addEventListener("click", function() {
+            _this.save();
+        });
     }
 }
--- src/assets/js/app.views.departament.index.js
+++ src/assets/js/app.views.departament.index.js
@@ -3,14 +3,30 @@ app.views.departament.index = function($el) {
     if (!$el) {
         throw("Element undefined");
     }
-
+    
     this.$el = $el;
+    this.db = new db("departaments");
+
     this.init();
 };
 
 app.views.departament.index.prototype = {
     init: function() {       
+        this.$elGrid = this.$el.parentElement.querySelector("[name='grid']");
+        
+        this.loadGrid();
         this.initEvents();
+    },
+
+    loadGrid: function() {
+        let elTBody = this.$elGrid.parentElement.querySelector("tbody");
+        
+        let tbody = this.db.get().reduce((row, departament) => {
+            row = row.concat("<tr><td>".concat(departament.id).concat("</td><td>").concat(departament.description).concat("</td></tr>"));
+            return row;
+        }, "");
+        
+        elTBody.innerHTML = tbody;
     },
 
     initEvents: function() {        
