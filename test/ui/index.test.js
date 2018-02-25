21c573cdaba51d9382619d34337335d55713cbf5
Willyam Santana Almeida <willyamalmeida@hotmail.com>
2018-02-25 01:31:05 -0300

    add test

---
+++ test/ui/index.test.js
@@ -0,0 +1,24 @@
+// const { Builder, By } = require("selenium-webdriver");
+// var chrome = require('selenium-webdriver/chrome');
+
+describe('My Company', () => {
+    // let _driver;
+
+    // before(function(){
+    //     debugger;
+    //     _driver = new Builder().forBrowser("chrome").build();
+    // });
+
+    // after(() => _driver && _driver.quit());
+
+    it("load page my company", function(){
+        // debugger;
+        // let url = "file:///".concat(__dirname).concat("dist/index.html");
+
+        // _driver.get(url);
+        // let header = _driver.findElement(By.css("header a")).innerText;
+
+        // expect(header).toEqual("My Company");
+        expect(true).toEqual(true);
+    });
+});
